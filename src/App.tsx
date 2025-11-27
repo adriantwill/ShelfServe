672980ed298b53a3f2097d467642d2fed429453f
adriantwill <atwill@asu.edu>
2025-11-27 11:02:47 -0500

    changed css for sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import "./App.css";
-import Sidebar from "./components/layout/sidebar";
+import Sidebar from "./components/layout/Sidebar";
 
 function App() {
   return (
     <>
-      <div className="flex h-full w-full">
-        {/* Sidebar - Left */}
+      <div className="flex w-screen">
         <Sidebar></Sidebar>
-
-        {/* Main Section - Middle */}
-        <main className="flex-4 bg-gray-100 ">
+        <main className="flex-10 bg-gray-100 ">
           <h1>Main Content</h1>
           {/* Main content */}
         </main>
-
         {/* Secondary Section - Right */}
-        <aside className="flex-2 bg-gray-200 ">
+        <aside className="flex-5 bg-gray-200 ">
           <h2>Secondary</h2>
           {/* Secondary content */}
         </aside>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+import logo from "../../assets/logo.png";
 
 export default function ComponentName() {
   return (
-    <div className="bg-blue-400 h-full w-9 rounded-r-md">
-      <div>
-        <h1 className="text-white">Shelf Selve</h1>
+    <div className="bg-blue-500 flex-row p-6 flex-4 h-screen w-9 rounded-r-lg">
+      <div className="flex items-center gap-4">
+        <img src={logo} className="size-10"></img>
+        <h1 className="text-white text-4xl font-bold">Shelf Selve</h1>
       </div>
+      <div className="items-end">Main Menu</div>
     </div>
   );
 }
