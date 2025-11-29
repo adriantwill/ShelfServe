1f1057e36d8df800fc525511b7c06ce207d243b2
adriantwill <atwill@asu.edu>
2025-11-29 11:03:53 -0500

    Fixed styling on main page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App() {
     <>
       <div className="flex w-screen">
         <Sidebar></Sidebar>
-        <main className="flex-10 bg-gray-100 p-5">
+        <main className="flex-10 p-5">
           <h1 className="text-5xl font-semibold">Good Morning Adrian</h1>
           <input
             type="text"
             placeholder="Search"
-            className="mt-4 px-4 py-2 bg-gray-300 text-gray-700 rounded-lg placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400 w-full"
+            className="mt-4 px-4 py-2 bg-gray-200 text-gray-700 rounded-lg placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400 w-full"
           />
         </main>
         {/* Secondary Section - Right */}
--- src/components/layout/SidebarButton.tsx
+++ src/components/layout/SidebarButton.tsx
@@ -14,7 +14,7 @@ export default function SidebarButton({
   return (
     <button
       onClick={onClick}
-      className="cursor-pointer text-2xl text-gray-300 hover:text-white hover:bg-blue-400/30 rounded-lg px-4 py-2 transition-all duration-200 flex items-center gap-3 w-full my-2 "
+      className="cursor-pointer text-2xl text-gray-200 hover:text-white hover:bg-blue-400/30 rounded-lg px-4 py-2 transition-all duration-200 flex items-center gap-3 w-full my-2 "
     >
       {Icon && <Icon />}
       <span>{children}</span>
