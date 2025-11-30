6ca58c3b0ff2ce7830c13d6f7b633c5600c2de7f
adriantwill <atwill@asu.edu>
2025-11-29 20:42:09 -0500

    unified tailwind colors for entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import "./App.css";
+import ListItem from "./components/layout/ListItem";
 import Sidebar from "./components/layout/Sidebar";
-
+import TestImage from "./assets/testItem.png";
 function App() {
   return (
     <>
-      <div className="flex w-screen">
+      <div className="flex w-screen dark:bg-gray-500">
         <Sidebar></Sidebar>
-        <main className="flex-10 p-5">
+        <main className="flex-10 p-5 border-r border-divider ">
           <h1 className="text-5xl font-semibold">Good Morning Adrian</h1>
           <input
             type="text"
             placeholder="Search"
-            className="mt-4 px-4 py-2 bg-gray-200 text-gray-700 rounded-lg placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400 w-full"
+            className="my-6 px-4 py-2 bg-input text-text-primary rounded-lg placeholder-text-secondary focus:outline-none focus:ring-2 focus:ring-accent w-full focus:bg-input-focus transition-all duration-100 ease-in-out"
           />
+          <div className="flex flex-col gap-4">
+            <ListItem
+              itemName="Milk"
+              expirationDate="Expires: Dec 5, 2025"
+              imageUrl={TestImage}
+              onCheckboxChange={(checked) => console.log(checked)}
+            />
+            <ListItem
+              itemName="Milk"
+              expirationDate="Expires: Dec 5, 2025"
+              imageUrl={TestImage}
+              onCheckboxChange={(checked) => console.log(checked)}
+            />
+          </div>
         </main>
         {/* Secondary Section - Right */}
-        <aside className="flex-5 bg-gray-200 ">
+        <aside className="flex-5 ">
           <h2>Secondary</h2>
           {/* Secondary content */}
         </aside>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,12 +5,12 @@ import { LuChefHat } from "react-icons/lu";
 
 export default function ComponentName() {
   return (
-    <div className="bg-blue-500 flex-row p-6 flex-4 h-screen w-9 rounded-r-lg">
+    <div className="bg-primary p-6 flex-4 h-screen w-9 rounded-r-lg">
       <div className=" flex items-center gap-4">
         <img src={logo} className="size-10"></img>
-        <h1 className="text-white text-4xl font-bold">Shelf Selve</h1>
+        <h1 className="text-text-inverse text-4xl font-bold">Shelf Selve</h1>
       </div>
-      <div className="mt-10 mb-3 px-4 text-gray-400">Main Menu</div>
+      <div className="mt-10 mb-3 px-4 text-text-muted">Main Menu</div>
       <SidebarButton icon={FaList} children={"Dashboard"}></SidebarButton>
       <SidebarButton icon={LuChefHat} children={"Recipe"}></SidebarButton>
     </div>
