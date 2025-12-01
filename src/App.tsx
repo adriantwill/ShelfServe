7f81c6be1695abbab71aff59f2401b346663e228
adriantwill <142526523+adriantwill@users.noreply.github.com>
2025-12-01 18:13:44 -0500

    shows which sidebar is selected visually

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import "./App.css";
 import ListItem from "./components/layout/ListItem";
 import Sidebar from "./components/layout/Sidebar";
+import SortDropdown from "./components/layout/SortDropdown";
 import TestImage from "./assets/testItem.png";
 function App() {
   return (
     <>
       <div className="flex w-screen dark:bg-gray-500">
         <Sidebar></Sidebar>
-        <main className="flex-10 p-5 border-r border-divider ">
+        <div className="flex-10 p-5 border-r border-divider ">
           <h1 className="text-5xl font-semibold">Good Morning Adrian</h1>
           <input
             type="text"
-            placeholder="Search here"
+            placeholder="Search"
             className="my-6 px-4 py-2 bg-input text-text-primary rounded-lg placeholder-text-secondary focus:outline-none focus:ring-2 focus:ring-accent w-full focus:bg-input-focus transition-all duration-100 ease-in-out"
           />
-          <div className="flex flex-col gap-4">
+          <SortDropdown
+            onSortChange={(option) => console.log("Sort by:", option)}
+          />
+          <div className="flex flex-col gap-4 mt-4">
             <ListItem
               itemName="Milk"
               expirationDate="Expires: Dec 5, 2025"
@@ -28,7 +31,7 @@ function App() {
               onCheckboxChange={(checked) => console.log(checked)}
             />
           </div>
-        </main>
+        </div>
         {/* Secondary Section - Right */}
         <aside className="flex-5 ">
           <h2>Secondary</h2>
