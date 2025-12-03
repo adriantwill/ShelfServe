fb794dc81558531a6993a8014f64b72c4265a5ed
adriantwill <142526523+adriantwill@users.noreply.github.com>
2025-12-02 22:14:41 -0500

    chaged teh sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,8 @@
-import ListItem from "./components/layout/ListItem";
-import Sidebar from "./components/layout/Sidebar";
-import SortDropdown from "./components/layout/SortDropdown";
-import TestImage from "./assets/testItem.png";
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <>
-      <div className="flex w-screen dark:bg-gray-500">
-        <Sidebar></Sidebar>
-        <div className="flex-10 p-5 border-r border-divider ">
-          <h1 className="text-5xl font-semibold">Good Morning Adrian</h1>
-          <input
-            type="text"
-            placeholder="Search"
-            className="my-6 px-4 py-2 bg-input text-text-primary rounded-lg placeholder-text-secondary focus:outline-none focus:ring-2 focus:ring-accent w-full focus:bg-input-focus transition-all duration-100 ease-in-out"
-          />
-          <SortDropdown
-            onSortChange={(option) => console.log("Sort by:", option)}
-          />
-          <div className="flex flex-col gap-4 mt-4">
-            <ListItem
-              itemName="Milk"
-              expirationDate="Expires: Dec 5, 2025"
-              imageUrl={TestImage}
-              onCheckboxChange={(checked) => console.log(checked)}
-            />
-            <ListItem
-              itemName="Milk"
-              expirationDate="Expires: Dec 5, 2025"
-              imageUrl={TestImage}
-              onCheckboxChange={(checked) => console.log(checked)}
-            />
-          </div>
-        </div>
-        {/* Secondary Section - Right */}
-        <aside className="flex-5 ">
-          <h2>Secondary</h2>
-          {/* Secondary content */}
-        </aside>
-      </div>
+      <Home></Home>
     </>
   );
 }
